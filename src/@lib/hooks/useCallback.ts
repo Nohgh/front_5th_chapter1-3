5cce5ea480acd0aa8af29cb9f69db65f679fb576
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-04-08 23:44:40 +0900

    Feat: memo, deepMemo

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,20 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-function-type */
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
 import { DependencyList } from "react";
-import { shallowEquals } from "../equalities";
-import { useRef } from "./useRef";
+import { useMemo } from "./useMemo";
 export function useCallback<T extends Function>(
   factory: T,
   _deps: DependencyList,
 ) {
-  const _savedDeps = useRef<null | DependencyList>(null);
-  const _savedFactory = useRef<null | T>(null);
-
-  if (_savedDeps.current === null) _savedDeps.current = _deps;
-  if (_savedFactory.current === null) _savedFactory.current = factory;
-
-  if (!shallowEquals(_deps, _savedDeps.current)) {
-    _savedDeps.current = _deps;
-    _savedFactory.current = factory;
-  }
-  return _savedFactory.current;
+  //useMemo를 이용합니다.
+  return useMemo(() => factory, _deps);
 }
