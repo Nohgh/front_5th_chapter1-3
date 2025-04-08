284f29be72efd002ff87d68c41f148f239594fe4
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-04-08 23:52:15 +0900

    Fix: memo.ts

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,6 +1,5 @@
 import { shallowEquals } from "../equalities";
 import { ComponentType, createElement, ReactNode } from "react";
-
 /**memo HOC는 컴포넌트의 props를 앝은 비교하여 불필요한 리렌더링을 방지합니다. */
 export function memo<P extends object>(
   Component: ComponentType<P>,
@@ -10,13 +9,12 @@ export function memo<P extends object>(
   let prevElement: ReactNode;
 
   return function MemoizedCompoenent(props: P) {
-    if (!prevProps && !_equals(prevProps, props)) {
-      const newElement = createElement(Component, props);
-      prevProps = props;
-      prevElement = newElement;
-      return newElement;
+    if (prevProps && _equals(prevProps, props)) {
+      return prevElement;
     }
-
-    return prevElement;
+    const element = createElement(Component, props);
+    prevProps = props;
+    prevElement = element;
+    return element;
   };
 }
