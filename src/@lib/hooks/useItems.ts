9047c754cfdbf33c46f538b4fbc5166448ae5419
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-04-10 01:18:17 +0900

    Refactor: hook/useItems -> @lib/hook

--- src/@lib/hooks/useItems.ts
+++ src/@lib/hooks/useItems.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { generateItems } from "../utils";
-import { useCallback } from "../@lib";
+import { generateItems } from "../../utils";
+import { useCallback } from "..";
 
 export const useItems = () => {
   const [items, setItems] = useState(generateItems(1000));
